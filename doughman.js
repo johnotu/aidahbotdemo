b2f33ca91cf41af0148a6f7a4c16e9767464fc0b
John Otu <johnne01@gmail.com>
2017-08-22 14:18:47 +0000

    Added backend and model files.
    Removed s stub of MSBot service file.
    Added env variables.
    Added route dashboard.
    Changes to be committed:
    	modified:   .env
    	deleted:    PostDeployScripts/runGulp.cmd
    	new file:   config/database.js
    	modified:   doughman.js
    	new file:   models/product.js
    	modified:   package.json
    	new file:   routes/dashboard.js

--- doughman.js
+++ doughman.js
@@ -3,9 +3,26 @@ require('dotenv').config();
 
 const express = require('express');
 const builder = require('botbuilder');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const dashboard = require('./routes/dashboard');
+
 const port = process.env.PORT || 3978;
 
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database);
+mongoose.set('debug', true);
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected to Doughman DB')
+});
+mongoose.connection.on('error', err => {
+	console.error('Doughman DB error: ', err);
+	throw err;
+})
+
 const app = express();
+
 
 let connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
@@ -21,6 +38,7 @@ bot.dialog('/', [
 ]);
 
 app.use(express.static('public'));
+app.use('/dashboard', dashboard);
 
 app.post('/api/messages', connector.listen());
 
